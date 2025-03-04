6805e23d4c5c877a9a39a13711c060c46106ca51
sikhouane <sikhouane@myges.fr>
2025-03-04 11:11:20 +0100

    add config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  content: ["./public/*.{html,js}","./views/*.twig"],
-  theme: {
-    extend: {},
+  s3: {
+    accessKeyId: 'AKIATG6MG4KV6F7QXNCE',
+    secretAccessKey: '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',
+    endpoint: 's3.eu-west-1.amazonaws.com', 
+    bucket: 'esgi-cloud-exam-bucket-81524', 
   },
-  plugins: [],
-}
+  suffix: {
+    small: '_small.jpg',
+    full: '_full.jpg'
+  }
+};
